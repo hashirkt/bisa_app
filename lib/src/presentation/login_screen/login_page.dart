ff0a2a086ed98895f17c581113a0a9c6c9413c96
Hashir KT <hashirkt3132@gmail.com>
2024-02-17 11:04:10 +0530

    initial commit

---
+++ lib/src/presentation/login_screen/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:bisa_app/src/utils/resources/colors.dart';
+import 'package:flutter/material.dart';
+
+class Login_Page extends StatelessWidget {
+  const Login_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        color: back_color,
+        height: double.infinity,
+        width: double.infinity,
+        child: const Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
